f5943e48d0ca51676479daaf0a9b0a74aadd0b8d
umfhero <67478293+umfhero@users.noreply.github.com>
2025-06-20 12:42:51 +0100

    3 items, wood sheet, hard wood and hardwood alloy in prep for mekanism

--- src/main/java/net/umf/blockmod/BlockMod.java
+++ src/main/java/net/umf/blockmod/BlockMod.java
@@ -1,5 +1,7 @@
 package net.umf.blockmod;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.umf.blockmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -30,11 +32,19 @@ public class BlockMod {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+
+        ModItems.register(modEventBus);
+
+
+
+
+
+
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -50,6 +60,15 @@ public class BlockMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.WOODSHEET);
+            event.accept(ModItems.HARDWOOD);
+            event.accept(ModItems.HARDWOOD_ALLOY);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/umf/blockmod/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.umf.blockmod.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.umf.blockmod.BlockMod;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BlockMod.MOD_ID);
+
+
+    public static final DeferredItem<Item> WOODSHEET = ITEMS.register("woodsheet",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> HARDWOOD = ITEMS.register("hardwood",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> HARDWOOD_ALLOY = ITEMS.register("hardwood_alloy",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
